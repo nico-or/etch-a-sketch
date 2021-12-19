8d663c634ebfff12b98d8f027ac53ab4ea8ca014
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-19 14:03:31 -0300

    Add cell darkening functionality

--- script.js
+++ script.js
@@ -10,11 +10,37 @@ const makeGrid = function (n) {
 
 // Cell darkening functionality
 let grid = document.querySelector(".grid");
-console.log(grid);
 
 grid.addEventListener('mouseover', e => {
     if (e.target.classList.contains("cell")) {
-        console.log(e);
+        let currentRGB = e.target.style.backgroundColor
+        let rgbArray = rgbString2Array(currentRGB);
+        let newRGB = map(rgbArray, reduceValue)
+        e.target.style.backgroundColor = rgbArray2String(newRGB);
     }
 });
 
+// applies rule() function over each element of array
+function map(array, rule) {
+    let mapArray =[];
+    for (item of array){
+        mapArray.push(rule(item));
+    };
+    return mapArray;
+}
+
+// reduces Value. intended to use with rgb values
+function reduceValue(value) {
+    return (value - 16).toFixed();
+}
+
+// turns rgb string into array
+function rgbString2Array(rgb) {
+    return rgb.match(/\d+/g);
+};
+
+// turns array into rgb string
+function rgbArray2String(rgb) {
+    if (rgb.length != 3) return;
+    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+}
